e26fc0f64957fea87cb86ac00b27489b314296ba
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-11-07 23:18:38 -0800

    Complete gets of number and string

--- index.rb
+++ index.rb
@@ -1,9 +1,8 @@
-puts "Hello World";
+# puts "Enter a number: ";
+# num1 = gets.chomp().to_f;
+# puts "Enter another number: ";
+# num2 = gets.chomp().to_f;
+# num3 = (num1 + num2);
+# puts num3;
 
-# END {
-#   puts "Last";
-# }
-
-# BEGIN {
-#   puts "Start";
-# }
+puts "Roses are {color}";
