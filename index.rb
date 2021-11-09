ebbf399faeeb056711cd32b142dbb7682ebff6a4
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-11-09 01:04:28 -0800

    Complete method

--- index.rb
+++ index.rb
@@ -8,7 +8,23 @@
 # Array
 # arrs = Array[3, false, "Germany", "Philippines"];
 # puts arrs[0, 3];
-arrs = Array.new();
-arrs[0] = "Mary";
-arrs[1] = "Gary";
-puts arrs.sort();
+# arrs = Array.new();
+# arrs[0] = "Mary";
+# arrs[1] = "Gary";
+# puts arrs.sort();
+
+# Hash 
+# states = {
+#   "BC" => "Victoria",
+#   :ON => "Toronto",
+#   "AB" => "Calgary"
+# }
+
+# puts states[:ON];
+
+# Method
+def sayHi(number)
+  return "Hello User " + number.to_s
+end
+
+puts sayHi(5)
