ea3307d77eae905330486f02457e8f170f8f9bec
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2021-11-08 23:24:14 -0800

    Complete array

--- index.rb
+++ index.rb
@@ -5,4 +5,10 @@
 # num3 = (num1 + num2);
 # puts num3;
 
-puts "Roses are {color}";
+# Array
+# arrs = Array[3, false, "Germany", "Philippines"];
+# puts arrs[0, 3];
+arrs = Array.new();
+arrs[0] = "Mary";
+arrs[1] = "Gary";
+puts arrs.sort();
